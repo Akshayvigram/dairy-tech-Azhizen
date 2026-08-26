57fb1e14202b4f177d341792b0ddd8d7431ca787
Karthikeyan130405 <165808643+Karthikeyan130405@users.noreply.github.com>
2026-08-26 21:31:12 +0530

    this is the changes from ensuring the safety of credentials

--- backend/firebaseAdmin.js
+++ backend/firebaseAdmin.js
@@ -25,12 +25,12 @@ import { getFirestore, collection, addDoc, serverTimestamp } from "firebase/fire
 
 // Your friend's Firebase Web Configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBDRX8tVe_SEbJ8zHGF6yyKJUvplk_BTGc",
-  authDomain: "dairy-tech-2411d.firebaseapp.com",
-  projectId:  "dairy-tech-2411d",
-  storageBucket: "dairy-tech-2411d.firebasestorage.app",
-  messagingSenderId: "989462243216",
-  appId: "1:989462243216:web:b6c5eba8371c6cf0ecf515"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
